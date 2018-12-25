871f1be17def808624bb2473afea4d67dc293b05
Kevin Stradtman <stradtkt@gmail.com>
2018-12-25 10:05:45 -0500

    Added the ability to add players

--- client/main.js
+++ client/main.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
-console.log(Players.find().fetch());
 
-const players = [{
-    _id: '1',
-    name: 'Lauren',
-    score: 99
-}, {
-    _id: '2',
-    name: 'Cory',
-    score: -1
-}, {
-    _id: '3',
-    name: 'Kevin',
-    score: 110
-}];
-
-const renderPlayers = function(playersList) {
-    return playersList.map(function(player) {
+const renderPlayers = (playersList) => {
+    return playersList.map((player) => {
         return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
     });
 };
 
-Meteor.startup(function() {
-    let title = 'Score Keep';
-    let name = "Kevin";
-    let jsx = (
-        <div>
-            <h1>{title}</h1>
-            <p>Hello {name}</p>
-            <p>This is my second paragraph</p>
-            {renderPlayers(players)}
-        </div>
-    );
+const handleSubmit = (e) => {
+    let playerName = e.target.playerName.value;
+    e.preventDefault();
+    if(playerName) {
+        e.target.playerName.value = '';
+        Players.insert({
+            name: playerName,
+            score: 0
+        });
+    }
+};
 
-    ReactDOM.render(jsx, document.getElementById('app'));
+Meteor.startup(() => {
+    Tracker.autorun(() => {
+        let players = Players.find().fetch();
+        let title = 'Score Keep';
+        let name = "Kevin";
+        let jsx = (
+            <div>
+                <h1>{title}</h1>
+                <p>Hello {name}</p>
+                <p>This is my second paragraph</p>
+                {renderPlayers(players)}
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="playerName" placeholder="Player Name"/>
+                    <button >Add Player</button>
+                </form>
+            </div>
+        );
+        ReactDOM.render(jsx, document.getElementById('app'));
+    });
 });
