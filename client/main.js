fe7b0d1c032626d291b380aa60d9d27a47d1c187
Kevin Stradtman <stradtkt@gmail.com>
2018-12-25 10:24:14 -0500

    Added remove functionality

--- client/main.js
+++ client/main.js
@@ -7,7 +7,12 @@ import {Players} from './../imports/api/players';
 
 const renderPlayers = (playersList) => {
     return playersList.map((player) => {
-        return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+        return (
+            <p key={player._id}>
+                {player.name} has {player.score} point(s).
+                <button onClick={() => Players.remove({_id: player._id})}>X</button>
+            </p>
+        );
     });
 };
 
@@ -27,12 +32,9 @@ Meteor.startup(() => {
     Tracker.autorun(() => {
         let players = Players.find().fetch();
         let title = 'Score Keep';
-        let name = "Kevin";
         let jsx = (
             <div>
                 <h1>{title}</h1>
-                <p>Hello {name}</p>
-                <p>This is my second paragraph</p>
                 {renderPlayers(players)}
                 <form onSubmit={handleSubmit}>
                     <input type="text" name="playerName" placeholder="Player Name"/>
